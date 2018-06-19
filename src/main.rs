2e93404fb4137a8aee1ad37bc97127a5f065b70b
Navidem <navid.emamdoost@gmail.com>
2018-06-18 20:10:29 -0700

    finished the dylib generation

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ extern crate regex;
 
 #[macro_use]
 extern crate quote;
-
-use proc_macro2::TokenStream;
-pub use std::{path, env};
+use quote::ToTokens;
+use proc_macro2::{TokenStream, Span, Ident};
+pub use std::{path, env, fs};
 pub use std::process::{Command, Stdio};
+use std::io::Write;
 
 mod rls;
 
@@ -25,21 +26,27 @@ fn main() -> Result<(), Box<std::error::Error>>{
     println!("Wroking path: {:?}", path);
     let funcs = run_analysis(path)?;
 
-    create_client_rs(funcs);
+    create_dylib_rs(funcs);
 
     Ok(())
 }
 
-fn create_client_rs(funcs: Vec<Def>) {
+fn create_dylib_rs(funcs: Vec<Def>) {
     let re_name = Regex::new(r"^fn (?P<name>.*?)\(").unwrap();
     let re_par_list = Regex::new(r"(?P<par>\w+): *(?P<ty>[^,)]+)").unwrap();
     let re_no_parm = Regex::new(r"^fn \w+ *\(\)").unwrap();
-    let mut _tokens = TokenStream::new();
+    let re_ret_val = Regex::new(r"-> *(?P<retTy>.[^{ ]*)").unwrap(); 
+    //TODO: I'm breaking on space as well as {, check it to see if there is space in return type like vec
+
+    let mut content = quote! {
+        extern crate userProjectLazy as lazy;
+    };
 
     for func in funcs {
-        let text = func.sig.expect("Panic! No sig!!").text;
+        let text = func.sig.expect("Panic! No sig!!").text; //"fn foo() -> f64  {}";//
         let mut parms: Vec<&str> = Vec::new();
         let mut types: Vec<&str> = Vec::new();
+        let ret_ts : Option<Ident>;
         println!("{}", text);
 
         let fn_name = re_name.captures(&text).expect("Panic! no fn_name!!").get(1).unwrap().as_str();
@@ -49,10 +56,60 @@ fn create_client_rs(funcs: Vec<Def>) {
                 types.push(caps.get(2).unwrap().as_str());
             } 
         }
-        //let parms = par_list.split(',').;
+        match re_ret_val.captures(&text) {
+            Some(x) => {
+                    let ret_val = x.get(1).unwrap().as_str();
+                    ret_ts = Some(Ident::new(ret_val, Span::call_site()));
+            },
+            None => ret_ts = None,
+        }
+        let ret_expression = match ret_ts {
+            Some(x) => quote! ( -> #x ),
+            None => quote! (),
+        };
+        //let ret_val = re_ret_val.captures(&text).unwrap().get(1).unwrap().as_str();
 
         println!("fn_name {}\nparms {:?}\ntypes {:?}", fn_name, parms, types);
         //let fn_name = &text.split(" ").next().unwrap().split('(').first();
 
+
+        let name = proc_macro2::Ident::new(fn_name, Span::call_site());
+        let mut parm_list_decl = TokenStream::new();
+        let mut parm_list_call = TokenStream::new();
+
+        for (idx, par) in parms.iter().enumerate() {
+            let pp = Ident::new(par, Span::call_site());
+            let tt = Ident::new(types[idx], Span::call_site());
+            quote!(
+                #pp: #tt, 
+            ).to_tokens(&mut parm_list_decl);
+
+            quote! (
+                #pp,   
+            ).to_tokens(&mut parm_list_call);
+
+        }
+        //let ret : Option<Ident> = Some(Ident::new(ret_val, Span::call_site()));
+        //println!("{}",parm_list );
+
+        quote!(
+            #[no_mangle]
+            pub extern "C" fn #name(#parm_list_decl)   #ret_expression {
+                lazy::#name(#parm_list_call)
+            }
+        ).to_tokens(&mut content);
+    } // end of forr on funcs
+    println!("{}", content);
+
+    let mut file = match fs::File::create("nvdDylib.rs") {
+        Err(oops) => panic! ("couldn't creat file! {}", oops),
+        Ok(fl) => fl,
+    };
+
+    let output = format!("{}", content);
+    match file.write_all(output.as_bytes()) {
+        Err(oops) => panic!("cannot write into file {}", oops),
+        Ok(_) => (),
     }
+
 }
